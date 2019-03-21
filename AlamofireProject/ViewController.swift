ddb358690ff462848a21434a9c4cdeeddadc72aa
nourelhouda <nourelhoudamiled1@gmail.com>
2019-03-21 09:38:09 +0100

    Initial Commit

---
+++ AlamofireProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlamofireProject
+//
+//  Created by MacBook Pro on 21/03/2019.
+//  Copyright © 2019 MacBook Pro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
